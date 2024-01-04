0163a8fa0ed0c8950a528b05e54ce48d4fbfa00e
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-01-04 21:37:23 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,27 @@
+function solution(n, lost, reserve) {
+    var answer = 0;
+
+    answer = n - lost.length;
+   lost.sort((a,b)=>a-b)
+ reserve.sort((a,b)=>a-b)
+     let lost2 = [...lost]
+     for(let l of lost){
+        if(reserve.includes(l)){
+            lost2.splice(lost2.indexOf(l),1)
+            reserve.splice(reserve.indexOf(l),1)
+            answer +=1;
+         
+        }
+     }
+    for(let l of lost2){
+         if(reserve.includes(l-1)){
+                reserve.splice(reserve.indexOf(l-1),1)
+                 answer+=1;
+         }else if(reserve.includes(l+1)){
+                reserve.splice(reserve.indexOf(l+1),1)
+                answer+=1;
+        }
+        
+    }
+    return answer;
+}
