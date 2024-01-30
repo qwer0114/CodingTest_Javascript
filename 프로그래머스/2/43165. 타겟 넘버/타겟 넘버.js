1a9aa331dd9f24bc6d473818204bc9379bdac40d
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-01-30 15:15:17 +0900

    [level 2] Title: 타겟 넘버, Time: 1.32 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,15 @@
+function solution(numbers, target) {
+    var answer = 0;
+    let sum = 0;
+    let n = numbers.length;
+    function DFS(L,sum){
+        if(L===n){
+            if(sum === target) answer+=1
+        }else{
+            DFS(L+1,sum+numbers[L])
+            DFS(L+1,sum-numbers[L])
+        } 
+    }
+    DFS(0,0)   
+    return answer;
+}
