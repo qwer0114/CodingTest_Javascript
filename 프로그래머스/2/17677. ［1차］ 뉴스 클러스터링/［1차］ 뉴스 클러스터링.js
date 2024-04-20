7e1d390ee5df4e84678f58574a729f5e332b8e79
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-04-20 14:52:54 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,36 @@
+function solution(str1, str2) {
+    var answer = 65536;
+    let string1 = str1.toUpperCase();
+    let string2 = str2.toUpperCase();
+    const regex = /^[A-Z]*$/;
+    let string1Array = [];
+    let string2Array = [];
+  function makeArray(string,array){
+     for(let i =0; i<string.length; i++){
+       let str = string[i]+ string[i+1]
+       if(regex.test(str))array.push(str)
+    }
+ }
+    
+    makeArray(string1, string1Array);
+    makeArray(string2, string2Array);
+    let intersection = [];
+    let union = [];
+    string1Array.map((str)=>{
+        if(string2Array.includes(str)){
+            union.push(str);
+            intersection.push(str)
+            string2Array.splice(string2Array.indexOf(str),1)
+        }else{
+            union.push(str);
+        }
+    })
+    string2Array.map((str)=>union.push(str))
+ 
+    let result = Math.floor((intersection.length / union.length) * 65536)
+    if(!union.length && !intersection.length){
+        return answer
+    }
+    return result
+}
+
