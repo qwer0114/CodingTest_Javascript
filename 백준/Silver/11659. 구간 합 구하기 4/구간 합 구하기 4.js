bef409652969eaf6d274187d4fe529a58e1913d9
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-04-21 13:26:28 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 364 ms, Memory: 48168 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -0,0 +1,16 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const arr = input[1].split(' ').map(v => +v);
+const cumsum = new Array(arr.length+1).fill(0);
+const output = [];
+
+arr.forEach((v, i) => {
+  cumsum[i+1] = cumsum[i] + v;
+});
+
+
+input.slice(2).forEach(ij => {
+  const [i, j] = ij.split(' ').map(v => +v);
+  output.push(cumsum[j]-cumsum[i-1]);
+});
+
+console.log(output.join('\n'));
