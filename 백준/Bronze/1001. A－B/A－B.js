d8be4e52068504e670d6b191a9ab73d75a0db48a
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-05-17 21:04:25 +0900

    [Bronze V] Title: A-B, Time: 104 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,7 @@
+const line = require("fs").readFileSync("/dev/stdin").toString();
+let input = line
+  .trim()
+  .split("\n")
+  .map((i) => i.replace("\r", ""));
+let array = input[0].split(" ");
+console.log(Number(array[0]) - Number(array[1]));
