59bad631db1d27c43af1fb54bb88f90562e2c49f
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-06-11 20:18:51 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,22 +1,13 @@
-function solution(str1, str2) {
-    var answer = 65536;
-    let string1 = str1.toUpperCase();
-    let string2 = str2.toUpperCase();
+ function makeArray(string,array){
     const regex = /^[A-Z]*$/;
-    let string1Array = [];
-    let string2Array = [];
-  function makeArray(string,array){
-     for(let i =0; i<string.length; i++){
+    for(let i =0; i<string.length; i++){
        let str = string[i]+ string[i+1]
        if(regex.test(str))array.push(str)
     }
- }
-    
-    makeArray(string1, string1Array);
-    makeArray(string2, string2Array);
-    let intersection = [];
-    let union = [];
-    string1Array.map((str)=>{
+}
+
+function clustering(string1Array, string2Array, union, intersection){
+ string1Array.map((str)=>{
         if(string2Array.includes(str)){
             union.push(str);
             intersection.push(str)
@@ -26,6 +17,25 @@ function solution(str1, str2) {
         }
     })
     string2Array.map((str)=>union.push(str))
+    }
+ 
+
+
+
+function solution(str1, str2) {
+    var answer = 65536;
+    let string1 = str1.toUpperCase();
+    let string2 = str2.toUpperCase();
+    const regex = /^[A-Z]*$/;
+    let string1Array = [];
+    let string2Array = [];
+    let intersection = [];
+    let union = [];
+    
+    makeArray(string1, string1Array);
+    makeArray(string2, string2Array);
+    
+    clustering(string1Array, string2Array, union, intersection)
  
     let result = Math.floor((intersection.length / union.length) * 65536)
     if(!union.length && !intersection.length){
