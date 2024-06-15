cc057f383946fa015133e61363e0398fd28cda95
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-06-15 12:40:01 +0900

    [level 2] Title: [1차] 캐시, Time: 0.56 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -2,19 +2,26 @@
 function solution(cacheSize, cities) {
     var answer = 0;
     let cache = [];
-    cities = cities.map((city)=>city.toUpperCase())
+    cities = cities.map((c)=>c.toUpperCase())
     
-    for(let i = 0; i<cities.length; i++){
-        // cache hit
-        if(cache.includes(cities[i])){
-            answer+=1;
-            cache.splice(cache.indexOf(cities[i]),1)
-            cache.push(cities[i])
-        }else{
-            answer+=5;
-            if(cache.length===cacheSize)cache.shift()
-            if(cache.length<cacheSize)cache.push(cities[i])
-        }
+    for(let i =0; i<cities.length; i++){
+       if(cache.includes(cities[i])){
+           let index = cache.indexOf(cities[i])
+           cache.splice(index,1)
+           cache.push(cities[i])
+           answer+=1
+       }else{
+           if(cache.length<cacheSize){
+               cache.push(cities[i])
+           }else{
+               if(cacheSize!==0){
+                    cache.shift()
+                    cache.push(cities[i])
+               }
+              
+           }
+           answer+=5;
+       }
     }
     return answer
 }
