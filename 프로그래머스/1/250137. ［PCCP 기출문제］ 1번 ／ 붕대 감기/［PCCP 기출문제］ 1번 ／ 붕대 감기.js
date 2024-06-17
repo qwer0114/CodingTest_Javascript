3c5b73d2e08f26c086680082605b79dffdd92a70
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-06-17 15:06:57 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,39 +1,39 @@
 function solution(bandage, health, attacks) {
-    // health -> 최대체력 attacks -> 몬스터의 공격 패턴
-    var answer = 0;
-    let time = bandage[0];
-    let heal = bandage[1];
-    let extraHeal = bandage[2];
-    let totalTime = attacks[attacks.length-1][0]
+    // bandage -> [heal 주기, 기본 회복량, 추가 회복량]
+    // health -> 최대 체력
+    // attacks -> 몬스터의 공격 패턴 [시간, 데미지] 배열
+    let [time, heal, extraHeal] = bandage;
+    let totalTime = attacks[attacks.length - 1][0];
     let currentHealth = health;
-    let success = 0;
-    let attackMap = new Map();
-    
-    attacks.map((attack)=>{
-        attackMap.set(attack[0],attack[1])
-    })
-    for(let i =0; i<=totalTime; i++){
-        if(attackMap.has(i)){
-            currentHealth -= attackMap.get(i)
-            success = 0;
-            continue;
+    let consecutiveHeals = 0;
+    let attackMap = new Map(attacks.map(attack => [attack[0], attack[1]]));
+
+    for (let i = 0; i <= totalTime; i++) {
+        if (attackMap.has(i)) {
+            currentHealth -= attackMap.get(i);
+            consecutiveHeals = 0;
+        } else {
+            if (currentHealth < health) {
+                currentHealth += heal;
+                consecutiveHeals++;
+            } else {
+                consecutiveHeals++;
+            }
+
+            if (consecutiveHeals === time) {
+                currentHealth += extraHeal;
+                consecutiveHeals = 0;
+            }
+
+            if (currentHealth > health) {
+                currentHealth = health;
+            }
         }
-        if(currentHealth>=health){
-            currentHealth = health
-            success++;
-            continue;
-        }else{
-            success++;
-            currentHealth += heal
+
+        if (currentHealth <= 0) {
+            return -1;
         }
-        if(success === time){
-            success = 0;
-            currentHealth += extraHeal
-        }
-        if(currentHealth<=0) return -1
     }
-    
-    
-    
-     return currentHealth <=0 ? -1 : currentHealth
+
+    return currentHealth > 0 ? currentHealth : -1;
 }
