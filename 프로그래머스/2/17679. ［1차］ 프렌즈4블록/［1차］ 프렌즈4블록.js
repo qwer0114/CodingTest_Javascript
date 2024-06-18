a14c9e1106bb6e6da4d54229f88fa78e20eea6d6
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-06-18 16:22:10 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 0.85 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -8,7 +8,8 @@ function solution(m, n, board) {
         // 2x2 블록 찾기
         for (let x = 0; x < board.length - 1; x++) {
             for (let y = 0; y < board[x].length - 1; y++) {
-                if (board[x][y] && board[x][y] === board[x+1][y] && board[x][y] === board[x][y+1] && board[x][y] === board[x+1][y+1]) {
+                if (board[x][y] && board[x][y] === board[x+1][y] && board[x][y] === board[x][y+1] 
+                    && board[x][y] === board[x+1][y+1]) {
                     remove.push([x, y]);
                 }
             }
@@ -21,22 +22,15 @@ function solution(m, n, board) {
         for (let i = 0; i < remove.length; i++) {
             let x = remove[i][0];
             let y = remove[i][1];
-            if (board[x][y]) {
-                board[x][y] = null;
-                answer++;
-            }
-            if (board[x+1][y]) {
-                board[x+1][y] = null;
-                answer++;
-            }
-            if (board[x][y+1]) {
-                board[x][y+1] = null;
-                answer++;
-            }
-            if (board[x+1][y+1]) {
-                board[x+1][y+1] = null;
-                answer++;
-            }
+            if (board[x][y] !== null) answer++;
+            if (board[x+1][y] !== null) answer++;
+            if (board[x][y+1] !== null) answer++;
+            if (board[x+1][y+1] !== null) answer++;
+
+            board[x][y] = null;
+            board[x+1][y] = null;
+            board[x][y+1] = null;
+            board[x+1][y+1] = null;
         }
 
         // 블록 떨어뜨리기
