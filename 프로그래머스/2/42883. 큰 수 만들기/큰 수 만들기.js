ab21b2959f90f6d4eec119fc93a111bace41d19f
SeongYeon <112809788+qwer0114@users.noreply.github.com>
2024-06-20 14:28:14 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,18 @@
+function solution(number, k) {
+    let stack = [];
+    let len = number.length;
+    
+    for (let i = 0; i < len; i++) {
+        let current = number[i];
+        while (k > 0 && stack.length > 0 && stack[stack.length - 1] < current) {
+            stack.pop();
+            k--;
+        }
+        stack.push(current);
+    }
+    
+    // 만약 k가 남아있다면 뒤에서부터 제거
+    stack.splice(stack.length - k, k);
+    
+    return stack.join('');
+}
