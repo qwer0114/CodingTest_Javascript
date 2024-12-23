08a1d9aa6c908e0ce222e20d4b3f0148cf9a0373
SeongYeon <rnalgh0114@gmail.com>
2024-12-23 14:12:18 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 96 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let input = fs
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((str) => str.replace(/\r/g, ""));
+
+function solution(input) {
+  return input.join("\n");
+}
+
+console.log(solution(input));
