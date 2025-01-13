e326f1e3beceb2a89bccb95c1317d87849678108
SeongYeon <rnalgh0114@gmail.com>
2025-01-13 09:21:39 +0900

    [level 2] Title: 소수 찾기, Time: 185.09 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,38 @@
+function solution(numbers) {
+    const set = new Set()
+    numbers = numbers.split("")
+    let visited = new Array(numbers.length).fill(false)
+    
+    function dfs(current){
+        if(current.length > 0 ){
+            const parse = Number(current);
+            if(isPrimeNumber(parse)) set.add(parse)
+            
+        }
+            for(let i = 0; i<numbers.length; i++){
+                if(!visited[i]){
+                    visited[i] = true;
+                    dfs(current+numbers[i])
+                    visited[i] = false;
+                }
+            }
+        
+    }
+    
+    dfs("")
+ 
+    return set.size;
+}
+
+function isPrimeNumber(number){
+    let target = Number(number);
+    
+    if(target<=1) return false;
+    for(let i = 2; i<target; i++){
+        if(target%i===0){
+           return false;
+        }
+    }
+    
+    return true
+}
