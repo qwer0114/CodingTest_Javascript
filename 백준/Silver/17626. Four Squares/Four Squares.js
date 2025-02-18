26c9f07e76e08323f7f770352d34dd9a32866a29
SeongYeon <rnalgh0114@gmail.com>
2025-02-18 21:06:30 +0900

    [Silver III] Title: Four Squares, Time: 220 ms, Memory: 10024 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((s) => s.split(" "));
+
+const n = Number(input[0]);
+
+function solution(n) {
+  const dp = new Array(n + 1).fill(4);
+  dp[0] = 0;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+    }
+  }
+
+  return dp[n];
+}
+
+console.log(solution(n));
