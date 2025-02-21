dddfa5b8799defc8b9d373d3f27571fb99a0dc1b
SeongYeon <rnalgh0114@gmail.com>
2025-02-21 16:33:19 +0900

    [level 3] Title: 네트워크, Time: 0.59 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,39 @@
+function solution(n, computers) {
+    var answer = 0;
+    let visited = Array(n+1).fill(false);
+    
+    const graph = makeGraph(computers,n)
+    
+    function dfs(node){
+        for(let next of graph[node]){
+            if(!visited[next]){
+                visited[next] = true
+                dfs(next)
+            }
+            
+        }
+    }
+    
+    for(let i = 1; i<=n; i++){
+        if(!visited[i]){
+            visited[i] = true
+             dfs(i)
+             answer+=1;
+        }
+          
+    }
+    
+    return answer;
+}
+
+function makeGraph(computers,n){
+    let graph = Array(n+1).fill().map(()=>[]);
+    for(let i = 0; i<computers.length; i++){
+      for(let j = 0; j<n; j++){
+          if(i===j) continue
+          else if(computers[i][j] === 1) graph[i+1].push(j+1)
+      }
+    }
+    
+    return graph
+}
