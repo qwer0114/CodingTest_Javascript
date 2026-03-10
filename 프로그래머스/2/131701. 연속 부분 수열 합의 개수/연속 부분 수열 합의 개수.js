0d322fed4a5f43d09bc0b2621e350658ebd26816
SeongYeon <rnalgh0114@gmail.com>
2026-03-10 15:06:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1130.13 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,14 +1,17 @@
 function solution(elements) {
-    const circular = [...elements,...elements];
-    const length = elements.length;
+    var answer = 0;
+    let doubled = [...elements,...elements];
+    let n = elements.length;
+    let set = new Set();
     
-    const set = new Set();
-    for(let i =0; i<length; i++){
-        for(let j = 0; j<length; j++){
-           const sum = circular.slice(j,j+i).reduce((acc,cur)=>acc+cur,0)
-           set.add(sum)
+    for(let i =1; i<=n; i++){
+        for(let index = 0; index<n; index++){
+            let sum = 0;
+            for(let k = index; k<index+i; k++){
+                sum+=doubled[k]
+            }
+            set.add(sum)
+        }
     }
-}
-   
     return set.size;
 }
