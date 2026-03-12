0bd26810a4b67696af7f033e8e35f32494f64953
SeongYeon <rnalgh0114@gmail.com>
2026-03-12 16:47:22 +0900

    [Silver II] Title: 랜선 자르기, Time: 180 ms, Memory: 12872 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -6,34 +6,25 @@ const input = fs
   .split("\n")
   .map((s) => s.split(" "));
 
-let [K, N] = input.shift().map(Number);
-let array = input.map(Number);
+const [K, N] = input.shift().map(Number);
+const lines = input.map((i) => Number(i[0])).sort((a, b) => a - b);
 
 function solution(K, N, lines) {
+  let min = 1;
+  let max = lines[lines.length - 1];
   let answer = 0;
-  let rt = Math.max(...lines);
-  let lt = 0;
+  while (min <= max) {
+    let mid = Math.floor((min + max) / 2);
+    let count = lines.reduce((acc, cur) => acc + Math.floor(cur / mid), 0);
 
-  function cutLine(cm, lines) {
-    let sum = 0;
-    for (let line of lines) {
-      sum += Math.floor(line / cm);
-    }
-    return sum;
-  }
-
-  while (lt <= rt) {
-    let mid = Math.floor((lt + rt) / 2);
-
-    if (cutLine(mid, lines) >= N) {
-      answer = mid;
-      lt = mid + 1;
+    if (count >= N) {
+      answer = Math.max(answer, mid);
+      min = mid + 1;
     } else {
-      rt = mid - 1;
+      max = mid - 1;
     }
   }
-
   return answer;
 }
 
-console.log(solution(K, N, array));
+console.log(solution(K, N, lines));
