61f3d7d0c12fe8e64820e5508377ff7adf682b1c
SeongYeon <rnalgh0114@gmail.com>
2026-03-15 17:12:13 +0900

    [level 2] Title: 피로도, Time: 9.14 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,19 +1,19 @@
 function solution(k, dungeons) {
-  let max = 0;
-  let visited = Array(dungeons.length).fill(false)
-  
-  function dfs(currentTired,count){
-      max = Math.max(max,count)
-      
-      for(let i = 0; i<dungeons.length; i++){
-          if(!visited[i] && currentTired>=dungeons[i][0]){
-              visited[i] = true;
-              dfs(currentTired-dungeons[i][1],count+1)
-              visited[i] = false;
-          }
-      }
-  }
+    let answer = 0;
+    let visited = Array(dungeons.length).fill(false);
     
-  dfs(k,0)
-  return max
+    function dfs(fatigue, count) {
+        answer = Math.max(answer, count); 
+        
+        for (let i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && fatigue >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(fatigue - dungeons[i][1], count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    dfs(k, 0);
+    return answer;
 }
