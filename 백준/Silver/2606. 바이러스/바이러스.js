15ad0b8dc0848b1cf6f2939cbb74e7895dd53503
SeongYeon <rnalgh0114@gmail.com>
2026-03-16 17:00:59 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,27 +1,42 @@
+const { count } = require("console");
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((s) => s.split(" "));
 
-const N = Number(input[0]);  
-const M = Number(input[1]);  
-const graph = Array(N + 1).fill().map(() => []);
-const visited = new Set();
+const N = input.shift().map(Number)[0];
+input.shift();
+const computers = input.map((i) => i.map(Number));
 
+function solution(N, computers) {
+  let answer = 0;
+  const graph = Array(N + 1)
+    .fill()
+    .map(() => []);
 
-for(let i = 2; i < M + 2; i++) {
-    const [a, b] = input[i].split(" ").map(Number);
+  const visited = Array(N + 1).fill(false);
+
+  for (let [a, b] of computers) {
     graph[a].push(b);
     graph[b].push(a);
-}
+  }
 
-function dfs(v) {
-    visited.add(v);
-    
-    for(let next of graph[v]) {
-        if(!visited.has(next)) {
-            dfs(next);
-        }
+  function dfs(v) {
+    for (let next of graph[v]) {
+      if (!visited[next]) {
+        visited[next] = true;
+        answer++;
+        dfs(next);
+      }
     }
+  }
+  visited[1] = true;
+  dfs(1);
+
+  return answer;
 }
 
-dfs(1);
-console.log(visited.size - 1);
+console.log(solution(N, computers));
