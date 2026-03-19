a9ed6bf570751ff172103cb2ba4c9b73bc754f2a
SeongYeon <rnalgh0114@gmail.com>
2026-03-19 17:16:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 24.66 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,29 +1,24 @@
 function solution(maps) {
     var answer = 0;
-    let directions = [[1,0],[0,1],[-1,0],[0,-1]]
-    let queue = [[0,0,1]];
+    let directions = [[1,0],[-1,0],[0,1],[0,-1]];
+    let queue = [[0,0,1]]
     let [targetX,targetY] = [maps.length-1,maps[0].length-1];
-    let visited = new Set(["0,0"])
+    maps[0][0] = 1
     
     while(queue.length>0){
-        let [currentX,currentY,walks] = queue.shift();
-        if(currentX===targetX && currentY===targetY) return walks;
+        let [x,y,walks] = queue.shift();
+        if(x === targetX && y === targetY) return walks
         
         for(let [dx,dy] of directions){
-            const nextX = currentX+dx;
-            const nextY = currentY+dy;
-            const key = `${nextX},${nextY}`
-            
-            if(nextX >= 0 && nextX < maps.length && 
-               nextY >= 0 && nextY < maps[0].length && 
-               maps[nextX][nextY] === 1 && 
-               !visited.has(key)) {
-                visited.add(key);
-                queue.push([nextX,nextY,walks+1]);
+            let nx = x+dx;
+            let ny = y+dy;
+            if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length && maps[nx][ny]===1){
+                maps[nx][ny] = 0
+                queue.push([nx,ny,walks+1])
             }
         }
-        
     }
     
-    return -1
+    
+    return -1;
 }
