7d79fa2384aaab0609985a5f75b0b85ca65514cc
SeongYeon <rnalgh0114@gmail.com>
2026-03-20 21:57:59 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 1020 ms, Memory: 22152 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -1,3 +1,4 @@
+const { count } = require("console");
 const fs = require("fs");
 const input = fs
   .readFileSync("./dev/stdin")
@@ -6,40 +7,33 @@ const input = fs
   .split("\n")
   .map((s) => s.split(" "));
 
-const [N, K] = input[0].map(Number);
+let [N, X] = input.shift().map(Number);
 
-function solution(N, K) {
-    // 시작점과 도착점이 같은 경우
-    if (N === K) return 0;
-    
-    // 방문 배열 (Set 대신 배열 사용)
-    const visited = Array(100001).fill(false);
-    // 0초 이동을 먼저 처리하기 위해 두 개의 큐 사용
-    const queue = [[N, 0]];
-    let index = 0;
-    
-    visited[N] = true;
-    
-    while (index < queue.length) {
-        const [pos, time] = queue[index++];
-        
-        // 순간이동 (0초) - 우선 처리
-        if (pos * 2 <= 100000 && !visited[pos * 2]) {
-            if (pos * 2 === K) return time;
-            visited[pos * 2] = true;
-            // 0초 이동은 큐의 앞쪽에 추가 (우선순위)
-            queue.splice(index, 0, [pos * 2, time]);
-        }
-        
-        // 걷기 (1초)
-        for (const next of [pos - 1, pos + 1]) {
-            if (next >= 0 && next <= 100000 && !visited[next]) {
-                if (next === K) return time + 1;
-                visited[next] = true;
-                queue.push([next, time + 1]);
-            }
-        }
+function solution(N, X) {
+  let dequeue = [[N, 0]];
+  let dist = Array(100001).fill(Infinity);
+
+  while (dequeue.length > 0) {
+    let [x, sec] = dequeue.shift();
+
+    if (x === X) return sec;
+
+    const nexts = [
+      [2 * x, 0],
+      [x + 1, 1],
+      [x - 1, 1],
+    ];
+
+    for (let [nextX, cost] of nexts) {
+      if (nextX < 0 || nextX >= 100001) continue;
+      const nextSec = sec + cost;
+      if (nextSec < dist[nextX]) {
+        dist[nextX] = nextSec;
+        if (cost === 0) dequeue.unshift([nextX, nextSec]);
+        else dequeue.push([nextX, nextSec]);
+      }
     }
+  }
 }
 
-console.log(solution(N, K));
+console.log(solution(N, X));
